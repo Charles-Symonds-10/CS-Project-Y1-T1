b98d0ffca560e7c42d9d66f2ca78327db1fff4bd
CS Project Y1 T1 <cs1112@exeter.ac.uk>
2022-11-05 22:30:08 +0000

    -Hourly Ave [DONE]

--- reporting.py
+++ reporting.py
@@ -72,7 +72,36 @@ def daily_median(data, monitoring_station, pollutant):
 def hourly_average(data, monitoring_station, pollutant):
     """Your documentation goes here"""
 
-    ## Your code goes here
+    pollution = {
+        'no': 2,
+        'pm10': 3,
+        'pm25': 4,
+    }
+    pol = pollution[pollutant]
+
+    with open(data + '/' + monitoring_station, 'r') as csv_file:
+        pol_data = []
+        for x in csv_file:
+            pol_data.append(x.split(','))
+
+        csv_file.close()
+
+        pol_data.pop(0)
+
+        hourly_ave = []
+        for x in pol_data:
+            if x[pol] != 'No data':
+                if x[1] not in hourly_ave:
+                    hourly_ave.append(x[1])
+                    hourly_ave.append([float(x[pol])])
+                else:
+                    hourly_ave[hourly_ave.index(x[1]) + 1].append(float(x[pol]))
+
+        all_hourly_ave = []
+        for x in range(0, len(hourly_ave), 2):
+            all_hourly_ave.append(np.round(np.average(hourly_ave[x + 1]), 4))
+
+        return all_hourly_ave
 
 
 def monthly_average(data, monitoring_station, pollutant):
@@ -99,4 +128,4 @@ def fill_missing_data(data, new_value, monitoring_station, pollutant):
     ## Your code goes here
 
 
-print(daily_median('data', 'Pollution-London Harlington.csv', 'no'))
+print(hourly_average('data', 'Pollution-London N Kensington.csv', 'no'))
