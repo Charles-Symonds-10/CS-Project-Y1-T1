a2e18d8d7f102e0b1bfbcdca0096735cc052ba1f
CS Project Y1 T1 <cs1112@exeter.ac.uk>
2022-11-23 11:24:48 +0000

    decided on how now just do

--- main.py
+++ main.py
@@ -1,30 +1,62 @@
-# This is a template. 
-# You should modify the functions below to match
-# the signatures determined by the project specification
+import reporting
+import pandas as pd
 
 
 def main_menu():
     """Your documentation goes here"""
-    # Your code goes here
+    run = True
+    while run:
+        choice = input('''
+    • R - Access the PR module
+    • I - Access the MI module
+    • M - Access the RM module
+    • A - Print the About text
+    • Q - Quit the application
+    : 
+    ''')
+        if type(choice) is str:
+            choice = choice.upper()
+            if choice == 'R':
+                reporting_menu()
+            elif choice == 'I':
+                monitoring_menu()
+            elif choice == 'M':
+                intelligence_menu()
+            elif choice == 'A':
+                about()
+            elif choice == 'Q':
+                run = quit()
+            else:
+                input('''That's not one of the options: [Press Enter to Continue]: ''')
+        else:
+            input('''That's not one of the options: [Press Enter to Continue]: ''')
+
+
+def reporting_menu():
+    data = {'Marylebone Road': pd.read_csv('data/Pollution-London Marylebone Road.csv'), 'N. Kensington': pd.read_csv('data/Pollution-London N Kensington.csv'), 'Harlington': pd.read_csv('data/Pollution-London Harlington.csv')}
+    pollutants = {'Nitric Oxide': 2, 'PM10': 3, 'PM2.5': 4}
+    choice = input('''
+
+''')
 
 def monitoring_menu():
     """Your documentation goes here"""
-    # Your code goes here
+    pass
 
 
 def intelligence_menu():
     """Your documentation goes here"""
-    # Your code goes here
+    pass
+
 
 def about():
     """Your documentation goes here"""
-    # Your code goes here
+    pass
+
 
 def quit():
     """Your documentation goes here"""
-    # Your code goes here
-
-
+    return False
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,28 +1,55 @@
-# This is a template. 
-# You should modify the functions below to match
-# the signatures determined by the project specification
-
-
 def sumvalues(values):
     """Your documentation goes here"""    
-    ## Your code goes here
+    total = 0
+    for x in values:
+        if type(x) is int or type(x) is float:
+            total += x
+        else:
+            return 'NON INT Error'
+    return total
 
 
 def maxvalue(values):
-    """Your documentation goes here"""    
-    ## Your code goes here
+    """Your documentation goes here"""
+    max_num = min
+    for x in values:
+        if type(x) is int or type(x) is float:
+            if x > max_num:
+                max_num = x
+        else:
+            return 'NON INT Error'
+    return max_num
 
 
 def minvalue(values):
-    """Your documentation goes here"""    
-    ## Your code goes here
+    """Your documentation goes here"""
+    min_num = max
+    for x in values:
+        if type(x) is int or type(x) is float:
+            if x < min_num:
+                min_num = x
+        else:
+            return 'NON INT Error'
+    return min_num
 
 
 def meannvalue(values):
-    """Your documentation goes here"""    
-    ## Your code goes here
+    """Your documentation goes here"""
+    num_of_values = 0
+    for x in values:
+        num_of_values += 1
+    total = sumvalues(values)
+    if type(total) is int:
+        mean_value = (total / num_of_values).__round__(4)
+        return mean_value
+    else:
+        return total
 
 
-def countvalue(values,xw):
-    """Your documentation goes here"""    
-    ## Your code goes here
+def countvalue(values, xw):
+    """Your documentation goes here"""
+    counter = 0
+    for x in values:
+        if x == xw:
+            counter += 1
+    return counter
